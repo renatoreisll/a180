a9c1b5a43dbbf5c4f9b2cb75cea24603cc0980c5
Renato Reis <renatoreis@live.com>
2020-12-14 15:35:37 -0400

    Alterações do arquivo ops.rb.

---
+++ ops.rb
@@ -0,0 +1,69 @@
+
+
+# # Operadores Matemáticos
+
+# def soma (n1, n2)
+#     puts n1 + n2
+# end
+
+# def subtrai (n1, n2)
+#     puts n1 - n2
+# end
+
+# def multiplica (n1, n2)
+#     puts n1 * n2
+# end
+
+# def divide (n1, n2)
+#     resultado = (n1.to_f / n2.to_f).round(2)
+#     puts resultado
+#     puts resultado.class
+# end
+
+# divide(10,3)
+
+# Operadores de Comparação
+
+def maior_que(v1, v2)
+    puts v1 > v2
+end
+
+# Evacando o método
+# maior_que(10,5)
+
+# maior_que(5,10)
+
+# maior_que(10,10)
+
+def menor_que(v1, v2)
+    puts v1 < v2
+end
+
+# menor_que(5,10)
+
+def maior_ou_igual_que(v1, v2)
+    puts v1 >= v2
+end
+
+# maior_ou_igual_que(10,10)
+
+# maior_ou_igual_que(5,10)
+
+def menor_ou_igual_que(v1, v2)
+    puts v1 <= v2
+end
+
+# menor_ou_igual_que(10,5)
+
+
+def igual(v1, v2)
+    puts v1 == v2
+end
+
+# igual(1,"10")
+
+def diferente(v1, v2)
+    puts v1 != v2
+end
+
+diferente(10,5)
