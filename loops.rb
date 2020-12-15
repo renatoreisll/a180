a238f4e55f1dc65eb9110a85cd1f5cf731ad2418
Renato Reis <renatoreis@live.com>
2020-12-15 11:57:38 -0400

    Inclusão do arquivo loops.rb.

---
+++ loops.rb
@@ -0,0 +1,17 @@
+# contador = 1
+
+# 10.times do
+#     puts "Execução número #{contador}"
+#     contador = contador + 1
+# end
+
+
+
+bandas = ["Aerosmith", "Só pra Contrariar", "Claudinho e Buchecha", "Guns N Roses"]
+
+contador = 0
+
+bandas.size.times do
+    puts bandas[contador]
+    contador = contador + 1
+end
